36970413d393a7772f7ea1c25ded179a4178505b
Jakub Musialowski <mds@mds.biz.pl>
2023-07-16 02:42:13 +0200

    Modified render function, button activity set

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const startButton = document.querySelector(".js-startButton");
 const stopButton = document.querySelector(".js-stopButton");
 const resetButton = document.querySelector(".js-resetButton");
-const sessionCounter = document.querySelector(".js-sessionCounter")
-const timeCounter = document.querySelector(".js-timeCounter");
 
 const numberFormat = (number) => {
     return number < 10 ? "0" + number : number;
-}
+};
 
-const timerDisplay = (minutes, seconds) => {
-    timeCounter.innerText = `${numberFormat(minutes)} : ${numberFormat(seconds)}`;
-}
+const timerDisplay = (minutes, seconds, sessionCount) => {
+    const timeCounter = document.querySelector(".js-timeCounter");
+    // const sessionCounter = document.querySelector(".js-sessionCounter");
+    timeCounter.innerHTML = `<p>${numberFormat(minutes)} : ${numberFormat(seconds)}</p>
+    <p>Liczba sesji: <span class="sessionCounter js-sessionCounter">${sessionCount}</span></p>`;
+};
 
 const timeCounting = (minutes, seconds, sessionCount) => {
     const countingInterval = setInterval(() => {
@@ -18,76 +19,86 @@ const timeCounting = (minutes, seconds, sessionCount) => {
         if(minutes === 0 && seconds === 0){
             clearInterval(countingInterval)
             sessionCount++;
-            sessionCounter.innerText = sessionCount;
+            // sessionCounter.innerText = sessionCount;
             minutes = 2;
             seconds = 0;
-            breakeCounting(minutes, seconds);
+            breakeCounting(minutes, seconds, sessionCount);
 
         } else 
             if(seconds === 0){
-                seconds = 59;
+                seconds = 19;
                 minutes--;
             } else {
                 seconds--;
             }
-        timerDisplay(minutes, seconds);
+        timerDisplay(minutes, seconds, sessionCount);
     }, 1000);
     stopCounting(countingInterval);
 };
 
-const breakeCounting = (minutes, seconds) => {
+const breakeCounting = (minutes, seconds, sessionCount) => {
     const breakeInterval = setInterval(() => {
         
         if(minutes === 0 && seconds === 0){
             clearInterval(breakeInterval);
             minutes = 5;
             seconds = 0;
-            timeCounting(minutes, seconds);
+            timeCounting(minutes, seconds, sessionCount);
         } else 
             if(seconds === 0){
-                seconds = 59;
                 minutes--;
+                seconds = 19;
             } else {
                 seconds--;
             }
-        timerDisplay(minutes, seconds);
-    }, 1000)
+        timerDisplay(minutes, seconds, sessionCount);
+    }, 1000);
     stopCounting(breakeInterval);
 };
 
 const startCounting = (minutes, seconds, sessionCount) => {
+
     startButton.addEventListener("click", () => {
         timeCounting(minutes, seconds, sessionCount);
+        startButton.setAttribute("disabled", "");
+        stopButton.removeAttribute("disabled");
     });
 };
 
 
 const stopCounting = (countingInterval, breakeInterval) => {
+
     stopButton.addEventListener("click", () => {
         clearInterval(countingInterval);
         clearInterval(breakeInterval);
+        stopButton.setAttribute("disabled", "");
+        startButton.removeAttribute("disabled");
+        resetButton.removeAttribute("disabled");
     });
 };
 
-const resetCouter = (minutes, seconds, sessionCount) => {
+const resetCounter = (minutes, seconds, sessionCount) => {
+
     resetButton.addEventListener("click", () => {
         minutes = 5;
         seconds = 0;
         sessionCount = 0;
-        timerDisplay(minutes, seconds)
-        sessionCounter.innerText = sessionCount;
-    })
-}
+        timerDisplay(minutes, seconds, sessionCount);
+
+        resetButton.setAttribute("disabled", "");
+
+    });
+};
 
 const init = () => {
     let minutes = 5;
     let seconds = 0;
-    timerDisplay(minutes, seconds);
-
     let sessionCount = 0;
-    sessionCounter.innerText = sessionCount;
+    timerDisplay(minutes, seconds, sessionCount);
+
     startCounting(minutes, seconds, sessionCount);
-    resetCouter(minutes, seconds, sessionCount);
-}
+    resetCounter(minutes, seconds, sessionCount);
+
+};
 
 init();
