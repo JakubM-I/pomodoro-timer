3592213770f74942ddcc0f0cee6e4c0882daa6dd
Jakub Musialowski <mds@mds.biz.pl>
2023-07-19 23:54:54 +0200

    Counting status added

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const startButton = document.querySelector(".js-startButton");
 const stopButton = document.querySelector(".js-stopButton");
 const resetButton = document.querySelector(".js-resetButton");
+const countingButtons = document.querySelector(".js-counterButtons");
+const statusCountingInfo = document.querySelector(".js-countingStatus");
 
-let pomodoroTime = 3;
+let pomodoroTime = 5;
 let shortBrakeTime = 1;
 let longBreakeTime = 2;
 let sessionsToLongBrake = 2;
 
+
+
 const numberFormat = (number) => {
     return number < 10 ? "0" + number : number;
 };
+
+const statusDisplay = (countingStatus) => {
+    statusCountingInfo.innerHTML = `Status: ${countingStatus}`;
+}
+
+statusDisplay();
+
+const toggleCountingStatus = () => {
+
+}
 
 const timerDisplay = (minutes, seconds, sessionCount) => {
     const timeCounter = document.querySelector(".js-timeCounter");
@@ -17,15 +31,16 @@ const timerDisplay = (minutes, seconds, sessionCount) => {
     <p>Liczba sesji: <span class="sessionCounter js-sessionCounter">${sessionCount} / ${sessionsToLongBrake}</span></p>`;
 };
 
-const timeCounting = (minutes, seconds, sessionCount) => {
+const timeCounting = (minutes, seconds, sessionCount, countingStatus) => {
     const countingInterval = setInterval(() => {
+        countingStatus = "pomodoro";
         
         if(minutes === 0 && seconds === 0){
             clearInterval(countingInterval)
             sessionCount++;
             minutes = sessionCount === sessionsToLongBrake ? longBreakeTime : shortBrakeTime;
             seconds = 0;
-            breakeCounting(minutes, seconds, sessionCount);
+            breakeCounting(minutes, seconds, sessionCount, countingStatus);
         } else 
             if(seconds === 0){
                 seconds = 19;
@@ -34,11 +49,13 @@ const timeCounting = (minutes, seconds, sessionCount) => {
                 seconds--;
             }
         timerDisplay(minutes, seconds, sessionCount);
+        statusDisplay(countingStatus);
     }, 1000);
     stopCounting(countingInterval);
 };
 
-const breakeCounting = (minutes, seconds, sessionCount) => {
+const breakeCounting = (minutes, seconds, sessionCount, countingStatus) => {
+    countingStatus = sessionCount === sessionsToLongBrake ? "long brake" : "short brake";
     const breakeInterval = setInterval(() => {
         if(minutes === 0 && seconds === 0){
             clearInterval(breakeInterval);
@@ -54,13 +71,14 @@ const breakeCounting = (minutes, seconds, sessionCount) => {
                 seconds--;
             }
         timerDisplay(minutes, seconds, sessionCount);
+        statusDisplay(countingStatus);
     }, 1000);
     stopCounting(breakeInterval);
 };
 
-const startCounting = (minutes, seconds, sessionCount) => {
+const startCounting = (minutes, seconds, sessionCount, countingStatus) => {
     startButton.addEventListener("click", () => {
-        timeCounting(minutes, seconds, sessionCount);
+        timeCounting(minutes, seconds, sessionCount, countingStatus);
         startButton.setAttribute("disabled", "");
         stopButton.removeAttribute("disabled");
     });
@@ -76,12 +94,15 @@ const stopCounting = (countingInterval, breakeInterval) => {
     });
 };
 
-const resetCounter = (minutes, seconds, sessionCount) => {
+const resetCounter = (minutes, seconds, sessionCount, countingStatus) => {
     resetButton.addEventListener("click", () => {
-        minutes = 5;
+        minutes = pomodoroTime;
         seconds = 0;
         sessionCount = 0;
         timerDisplay(minutes, seconds, sessionCount);
+
+        countingStatus = "pomodoro";
+        statusDisplay(countingStatus);
 
         resetButton.setAttribute("disabled", "");
     });
@@ -91,10 +112,13 @@ const init = () => {
     let minutes = pomodoroTime;
     let seconds = 0;
     let sessionCount = 0;
+
+    let countingStatus = "pomodoro";
+    statusDisplay(countingStatus);
     
     timerDisplay(minutes, seconds, sessionCount);
-    startCounting(minutes, seconds, sessionCount);
-    resetCounter(minutes, seconds, sessionCount);
+    startCounting(minutes, seconds, sessionCount, countingStatus);
+    resetCounter(minutes, seconds, sessionCount, countingStatus);
 };
 
 init();
